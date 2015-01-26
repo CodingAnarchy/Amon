b8af1ff093899831e65f90b60c60060520872a1a
CodingAnarchy <mtanous22@gmail.com>
2015-01-25 22:33:31 -0800

    Update utils library with comments on comma_sep_list function

--- lib/utils.py
+++ lib/utils.py
@@ -4,7 +4,9 @@ import inspect
 
 
 def comma_sep_list(lst):
+    """Set up string or list to URL list parameter format"""
     if not isinstance(lst, basestring):
+        # Convert list to proper format for URL parameters
         if len(lst) > 1:
             try:
                 lst = ','.join(lst)[:-1]
@@ -12,6 +14,7 @@ def comma_sep_list(lst):
                 raise Exception(inspect.stack()[1][3] + "expects a comma separated string or list object.")
         else:
             lst = lst[0]
-    elif not match(r'(\w+)?\w+', lst):
+    # Verify that strings passed in have the appropriate format (comma separated string)
+    elif not match(r'(\w+,)?\w+', lst):
         raise Exception("User list not properly formatted: expected list or comma separated string")
     return lst
