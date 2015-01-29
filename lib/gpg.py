09417e5a8ee5facceb52ed4c0afa7ba49d7dbac0
CodingAnarchy <mtanous22@gmail.com>
2015-01-28 17:23:51 -0800

    Update GPG library to encrypt a file if provided to the encrypt_msg function

--- lib/gpg.py
+++ lib/gpg.py
@@ -1,6 +1,8 @@
 # GPG utility library
-import gnupg
 from os import system
+
+import gnupg
+
 
 # clean GPG test directory on startup before proceeding with test
 system('rm -rf /home/testgpguser/gpghome')
@@ -9,13 +11,14 @@ gpg = gnupg.GPG(gnupghome='/home/testgpguser/gpghome')
 
 def import_keys(keys):
     print "Importing keys..."
+    result = None
     if isinstance(keys, basestring):
         result = gpg.import_keys(keys)
     elif isinstance(keys, list):
         for k in keys:
             result = gpg.import_keys(k)
     elif isinstance(keys, file):
-        key_data = open(keys).read()
+        key_data = keys.read()
         result = gpg.import_keys(key_data)
     return result
 
@@ -33,16 +36,25 @@ def list_keys(secret=False):
 
 def encrypt_msg(msg, to):
     print "Encrypting msg..."
+    enc_str = None
     if isinstance(msg, file):
-        # TODO: Encrypt file path
-        return None
+        with msg:
+            # TODO: put output file in same path as input file
+            status = gpg.encrypt_file(msg, recipients=to, always_trust=True, output='enc_msg.txt')
+            if not status.ok:
+                raise Exception("GPG encryption error: \nstatus: " + status.status + '\nstderr: ' + status.stderr)
+            with open('enc_msg.txt', 'rb') as enc:
+                enc_str = enc.read()
+                # TODO: Query user for deletion of original and/or encrypted file, if desired
     else:
         # treat msg as plaintext string
         enc_data = gpg.encrypt(msg, to, always_trust=True)
         enc_str = str(enc_data)
         if not enc_str.ok:
             raise Exception("GPG encryption error: \nstatus: " + enc_str.status + '\nstderr: ' + enc_str.stderr)
-        return enc_str
+    if enc_str is None:
+        raise Exception("GPG encryption failed to encrypt a message.")
+    return enc_str
 
 
 def decrypt_msg(enc, pw):
