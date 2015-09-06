ee24b8b57bc73947cd5140aca15389861b33ab00
CodingAnarchy <mtanous22@gmail.com>
2015-09-05 22:24:53 -0600

    Update Qt gui to have status bar and menu bar

--- gui/qt.py
+++ gui/qt.py
@@ -16,3 +16,30 @@ class Amon(QtGui.QMainWindow):
         self.keybase_user = KeybaseUser()
         self.gmail = GmailUser()
         self.address_book = AddressBook()
+
+        self.initUI()
+
+    def initUI(self):
+        exitAction = QtGui.QAction(QtGui.QIcon('exit.png'), '&Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(QtGui.qApp.quit)
+
+        menubar = self.menuBar()
+        menubar.setNativeMenuBar(False)
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+
+        self.statusBar().showMessage('Ready')
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Amon ' + AMON_VERSION)
+        self.show()
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    amon = Amon()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
